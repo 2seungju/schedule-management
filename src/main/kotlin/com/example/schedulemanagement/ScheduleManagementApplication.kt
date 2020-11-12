c83433bb2cf34ef1f58147139c7ca1c93b0b02c6
unknown <a01065253918@gmail.com>
2020-11-12 23:38:39 +0900

    Change Kotlin

---
+++ src/main/kotlin/com/example/schedulemanagement/ScheduleManagementApplication.kt
@@ -0,0 +1,11 @@
+package com.example.schedulemanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScheduleManagementApplication
+
+fun main(args: Array<String>) {
+    runApplication<ScheduleManagementApplication>(*args)
+}
